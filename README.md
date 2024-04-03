6409d1e90e1a6d202a8a799e882912757ffe2654
Jeax97 <97027891+Jeax97@users.noreply.github.com>
2024-04-03 12:57:22 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# D2AD
-Discord to Active Directory
+
+ Discord 2 Active Directory
+ 
+ Thanks for checking out my project, feel free to reach out on Discord J3ax#8367 in case of help needed with this integration, please remember this is just a small 
+ script, I'm working to make available the full integration
+ 
+ This script will fetch some data from the machine it is running on.
+ Running 2 powershell commands it will determine if the machine is enrolled in an Active Directory Domain and which
+ 
+ After determining the enrollment and the name, it will send an embed message to a specific channel and display correctly the domain name and the enrollment
+ status.
---
+++ index.js
@@ -0,0 +1,127 @@
+/*
+
+         88  ad888888b,                     88  
+         88 d8"     "88                     88  
+         88         a8P                     88  
+ ,adPPYb,88      ,d8P"  ,adPPYYba,  ,adPPYb,88  
+a8"    `Y88    a8P"     ""     `Y8 a8"    `Y88  
+8b       88  a8P'       ,adPPPPP88 8b       88  
+"8a,   ,d88 d8"         88,    ,88 "8a,   ,d88  
+ `"8bbdP"Y8 88888888888 `"8bbdP"Y8  `"8bbdP"Y8  
+                                                
+
+ Thanks for checking out my project, feel free to reach out on Discord J3ax#8367 in case of help needed with this integration, please remember this is just a small script, I'm working on the full integration
+
+ */
+
+const { pseudoRandomBytes } = require("crypto");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const Shell = require ('node-powershell');
+const { checkServerIdentity } = require("tls");
+
+const powershell = new Shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
+});
+
+const generator = require('generate-password');
+
+client.on("ready", () => {
+    console.log(`Spawned as ${client.user.tag}!`);
+    console.log(`[INFO] Displaying Active Directory status in General channel`);
+console.log("    _  _____           _ ");
+console.log("    | |/ __  \         | |");
+console.log("  __| |`' / /' __ _  __| |");
+console.log(" / _` |  / /  / _` |/ _` |");
+console.log("| (_| |./ /__| (_| | (_| |");
+console.log(" \__,_|\_____/\__,_|\__,_|");
+                          
+
+
+powershell.addCommand("(Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain");
+powershell.invoke().then(result => {
+    
+    var enrolled = result
+    if (result = true){
+        console.log("[INFO] The server running Discord Bot is correctly enrolled into an Active Directory Domain");
+        powershell.clear();
+        powershell.addCommand("(Get-WmiObject WIN32_ComputerSystem).Domain");
+        powershell.invoke().then(result2 => {
+            var domainName = result2
+            const botSpawn = new Discord.MessageEmbed()
+            .setColor('GREEN')
+            .setTitle('Active Directory Integration is up and running')
+            .addField('Domain Name:',`${String(domainName)}`, true)
+            .addField('Enrolled:', `${String(enrolled)}`, true)
+            client.channels.cache.get("875114498191667240").send(botSpawn);
+
+        })
+    } 
+
+    if (result = false){
+
+        console.log("[ERROR] The computer running the bot is not enrolled into an Active Directory Domain");
+    }
+
+
+
+}).catch(err=> {console.log(err);});
+
+
+});
+
+
+
+client.on("message", msg => {
+    if (msg.content === "!ping") {
+      msg.reply("pong");
+    }
+
+    if (msg.content === "!onboardme"){
+
+        var userpassword = generator.generate({
+            length: 15,
+            numbers: true,
+            symbols: true
+        })
+        var username = msg.author.username;
+        console.log("[INFO] Onboarding user into Active Directory Domain");
+
+        powershell.clear();
+        powershell.addCommand("(Get-WmiObject WIN32_ComputerSystem).Domain");
+        powershell.invoke().then(domainName =>{
+            powershell.clear()
+            var niceDomainName = domainName.replace(/(\r\n|\n|\r)/gm, "");
+            powershell.addCommand(`New-ADUser -Name "${username}" -GivenName "${username}" -Surname "Discord" -SamAccountName "${username}" -AccountPassword(ConvertTo-SecureString "${userpassword}" -AsPlainText -Force) -UserPrincipalName "${username}@${niceDomainName}" -Description "This account has been generated from Discord with the !onboardme command" -Enabled $true `)
+            powershell.invoke().then(outcome => {
+                msg.react('✅');
+                if (outcome){( console.log("[INFO] Powershell generated an outcome:" + outcome))} else {console.log("[INFO] Command run succesfully, no outcome provided by Powershell")};          
+                msg.author.send('Your Active Directory Account has been succesfully created, please login using \n Username: `' + username + '`\n Password: `' + userpassword + '` \n Thanks for using D2AD! ')
+            })
+        })
+
+        }
+
+
+        client.on('guildMemberUpdate', (oldMember, newMember) => {
+            // Fire if user has a role
+            if (newMember.roles.cache.some(r => r.name === "AD_AdministratorRole")) {
+              try {
+               console.log('[INFO] RoleAdministrator Detected!')
+               powershell.clear();
+               powershell.addCommand("Add-ADGroupMember -Identity GS_AdministratorRole -Members "+ newMember.user.username)
+               powershell.invoke();
+               console.log('[INFO] Group GS_AdministratorRole added in Active Directory!')
+              } catch(err) {
+                console.log('[ERROR] Powershell Error: ' +err);
+              }
+            }
+          })
+    })
+
+    
+
+
+  client.login("BOT-TOKEN-HERE");
+
